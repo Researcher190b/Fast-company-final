b3e3522862939d21fd1f651789842536b5ac51c0
Alexey Nikulin <researcher190b@gmail.com>
2022-11-12 20:39:13 +0300

    exercise #2 - users settings

--- src/app/components/ui/profession.jsx
+++ src/app/components/ui/profession.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-// import { useProfessions } from "../../hooks/useProfession";
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import {
     getProfessionById,
     getProfessionsLoadingStatus
 } from "../../store/professions";
-import PropTypes from "prop-types";
 
 const Profession = ({ id }) => {
-    // const { isLoading, getProfession } = useProfessions();
-    // const prof = getProfession(id);
     const isLoading = useSelector(getProfessionsLoadingStatus());
     const prof = useSelector(getProfessionById(id));
     if (!isLoading) {
